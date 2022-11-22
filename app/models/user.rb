c6c574cb5b1f8d305902aabc6d9737a708ceb82c
Aneal Laryea <aneallaryea100@gmail.com>
2022-11-22 13:41:57 +0000

    Fix errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :name, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
 end
