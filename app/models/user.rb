90914d52bfbdb71ad795c7c4e0ef992985bbfa5f
Aneal Laryea <aneallaryea100@gmail.com>
2022-11-23 05:29:42 +0000

    Update devise on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :timeoutable, :trackable
+         :recoverable, :rememberable, :validatable
 
   has_many :entities, dependent: :destroy
   has_many :groups, dependent: :destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   
   resources :groups
   resources :entities
