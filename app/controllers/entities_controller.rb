3cfe090e503fc44a8eb049170c34a4a742cf5999
Aneal Laryea <aneallaryea100@gmail.com>
2022-11-23 14:03:59 +0000

    Update entity controller

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,6 +1,6 @@
 class EntitiesController < ApplicationController
   before_action :set_entity, only: %i[show edit update destroy]
- before_action :set_group, only: %i[create]
+  before_action :set_group, only: %i[create]
 
   # GET /entities or /entities.json
   def index
