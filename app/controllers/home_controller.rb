1c01d0534536cb9a46cfe3241250858473a00302
Aneal Laryea <aneallaryea100@gmail.com>
2022-11-25 19:11:26 +0000

    update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def index
-    # redirect_to groups_path if user_signed_in?
     if user_signed_in?
       redirect_to groups_path
       @users = current_user.name
+    else
+      @users = 'Guest'
     end
   end
 end
